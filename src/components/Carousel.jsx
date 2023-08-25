5deaa15430ea79dcd4658423b123baf5095685cd
scypurple <scypurple@gmail.com>
2023-08-25 23:27:49 +0800

    refactoring to function component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,33 +1,33 @@
-import { Component } from "react";
-
-class Carousel extends Component {
-  state = {
-    active: 0,
+import { useState } from "react";
+export default function Carousel({ images }) {
+  const [active, setActive] = useState(0);
+  const handleIndexClick = (event) => {
+    setActive(+event.target.dataset.index);
   };
 
-  static defaultProps = {
-    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
-  };
-
-  render() {
-    const { active } = this.state;
-    const { images } = this.props;
-
-    return (
-      <div className="carousel">
-        <img src={images[active]} alt="animal hero" />
-        <div className="carousel-smaller">
-          {images.map((photo, index) => (
-            <img
-              key={photo}
-              src={photo}
-              className={index === active ? "active" : ""}
-              alt="animal thumbnail"
-            />
-          ))}
-        </div>
+  return (
+    <div className="carousel">
+      <img
+        src={images[active] || Carousel.defaultProps.images[0]}
+        alt="animal hero"
+      />
+      <div className="carousel-smaller">
+        {images.map((photo, index) => (
+          // eslint-disable-next-line
+          <img
+            onClick={handleIndexClick}
+            data-index={index}
+            key={photo}
+            src={photo}
+            className={index === active ? "active" : ""}
+            alt="animal thumbnail"
+          />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
-export default Carousel;
+
+Carousel.defaultProps = {
+  images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+};
