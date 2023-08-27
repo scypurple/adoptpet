49a405b2a6991c0899ccaf39666cf249c92d6ab4
scypurple <scypurple@gmail.com>
2023-08-27 20:55:38 +0800

    fix https

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -29,5 +29,5 @@ export default function Carousel({ images }) {
 }
 
 Carousel.defaultProps = {
-  images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  images: ["https://pets-images.dev-apis.com/pets/none.jpg"],
 };
--- src/fetch/fetchSearch.js
+++ src/fetch/fetchSearch.js
@@ -1,7 +1,7 @@
 const fetchSearch = async ({ queryKey }) => {
   const { animal, location, breed } = queryKey[1];
   const apiRes = await fetch(
-    `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`,
+    `https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`,
   );
   if (!apiRes.ok) {
     throw new Error(`pets not found for ${animal} ${location} ${breed}`);
